efa7aa78e92f2c82f1716f7d661f958312bb901e
Artem <stuzukartem12@gmail.com>
2026-07-16 16:41:06 +0300

    done2

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const baseURL =
-  (process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000") + "/api";
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 export const api = axios.create({
-  baseURL,
+  baseURL: `${baseURL}/api`,
   withCredentials: true,
 });
